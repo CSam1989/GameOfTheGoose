75e432edce7a923a1cc7d49e9d16f6da5537dcef
sam <sam@SAM2016>
2020-09-15 16:18:12 +0200

    Created a factory for all models

---
+++ Application/Common/Factories/ModelFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Application.Common.Interfaces;
+using Application.Common.Settings;
+using Application.Models;
+using Application.SpecialSpaces;
+
+namespace Application.Common.Factories
+{
+    public class ModelFactory : IModelFactory
+    {
+        private readonly AppConfig _config;
+
+        public ModelFactory(AppConfig config)
+        {
+            this._config = config;
+        }
+
+        public Game CreateGame()
+        {
+            return new Game();
+        }
+
+        public Board CreateBoard()
+        {
+            return new Board();
+        }
+
+        public Space CreateSpace(int spaceNumber)
+        {
+            foreach (var specialSpace in _config.Spaces.SpecialSpaces)
+                if (specialSpace.Space == spaceNumber)
+                    return (Space)Activator.CreateInstance(
+                        Type.GetType($"Application.SpecialSpaces.{specialSpace.Name}"),
+                        new object[] { spaceNumber });
+
+            return _config.Spaces.GoozeSpaces.Any(goosePlace => goosePlace == spaceNumber) ? new Goose(spaceNumber) : new Space(spaceNumber);
+        }
+
+        public Player CreatePlayer(string name)
+        {
+            return new Player(name);
+        }
+    }
+}
---
+++ Application/Common/Interfaces/IModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using Application.Models;
+
+namespace Application.Common.Interfaces
+{
+    public interface IModelFactory
+    {
+        Models.Game CreateGame();
+        Board CreateBoard();
+        Space CreateSpace(int spaceNumber);
+        Player CreatePlayer(string name);
+    }
+}

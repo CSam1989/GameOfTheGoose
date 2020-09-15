0e88cbff851a503214610b3c6e3e665f1fc0107b
sam <sam@SAM2016>
2020-09-15 14:57:31 +0200

    Added appsettings + moved app initiation to App.cs & IOC Container to program

---
+++ Presentation_Console/App.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation_Console
+{
+    public class App
+    {
+        public App()
+        {
+        }
+
+        // Equivalent to Main in Program.cs
+        public void Run()
+        {
+            //Adds global exception handling
+            //AppDomain.CurrentDomain.UnhandledException += _exceptionHandler.UnhandledExceptionTrapper;
+
+        }
+    }
+}
--- Presentation_Console/Program.cs
+++ Presentation_Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Presentation_Console
 {
@@ -6,7 +8,44 @@ namespace Presentation_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            //Add services from other layers
+            //services.AddApplication();
+
+            //Add Settings to IOC Container
+            var config = LoadConfiguration();
+            // IConfiguration to IOC Container
+            services.AddSingleton(config);
+
+            // AppConfig to IOC Container
+            //var appConfig = config.GetSection("BoardSettings").Get<AppConfig>();
+            //services.AddSingleton(appConfig);
+
+            //Add Services To IOC Container
+            //services.AddTransient<IInputService, InputService>();
+
+            services.AddTransient<App>();
+
+            return services;
+        }
+
+        private static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+
+            return builder.Build();
         }
     }
 }

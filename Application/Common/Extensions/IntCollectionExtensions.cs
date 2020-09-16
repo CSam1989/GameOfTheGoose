a9e301e441d80a4acf4426dfd808b2fd8b6f66f6
sam <sam@SAM2016>
2020-09-16 09:27:41 +0200

    Extension method to print out the dices

---
+++ Application/Common/Extensions/IntCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Application.Common.Extensions
+{
+    public static class IntCollectionExtensions
+    {
+        public static string DiceThrowsToString(this int[] numbers)
+        {
+            var output = numbers.Aggregate(
+                "",
+                (current, number) =>
+                    current + $"{number}+");
+
+            return output.Substring(0, output.Length - 1);
+        }
+    }
+}

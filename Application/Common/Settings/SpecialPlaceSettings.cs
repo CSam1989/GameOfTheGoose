7b84092cdd6d51cf22c5e1cc651f04149dfe4e3d
sam <sam@SAM2016>
2020-09-16 09:53:50 +0200

    implemented bridge special space

--- Application/Common/Settings/SpecialPlaceSettings.cs
+++ Application/Common/Settings/SpecialPlaceSettings.cs
@@ -9,8 +9,11 @@ namespace Application.Common.Settings
         /// <summary>SpecialThrow gives an array which contains an array with values (0: throw / 1: dice 1 / 2: dice 2 / 3: position to go to)</summary>
         public static readonly int[][] SpecialGooseThrows =
         {
+            //0: throw / 1: dice 1 / 2: dice 2 / 3: position to go to
             new int[] { 1, 5, 4, 26 },
             new int[] { 1, 6, 3, 53 }
         };
+
+        public const int BridgeToGoSpace = 12;
     }
 }
